a693459480c6950dbecc730ff319786c083b59ae
Junoh Moon <mjo970625@gmail.com>
2019-11-06 22:39:13 +0900

    Polish A_Identifier

--- A_Identifier.cpp
+++ A_Identifier.cpp
@@ -1,8 +1,11 @@
-#include <typeinfo>
 #include "A_Identifier.h"
+#include <typeinfo>
+#include <iostream>
+
+using std::cout;	using std::endl;
 
 void A_Identifier::Print(int d)
 {
 	indent(d);
-	cout<<"identifier: "<<name<<endl;
+	cout << "identifier: " << name << endl;
 }
--- A_Identifier.h
+++ A_Identifier.h
@@ -1,11 +1,14 @@
 #pragma once
 
 #include "A_Top.h"
+#include <string>
+
+using std::string;
 
 class A_Identifier : public A_Top {
 private:
-	std::string name;
+	string name;
 public:
-	A_Identifier(std::string& n) : name(n) {}
+	A_Identifier(const string& n) : name(n) {}
 	virtual void Print(int d);
 };
