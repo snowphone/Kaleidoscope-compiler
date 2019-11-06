d156b1dd2bd225229727d64036032f92d1cd032b
Junoh Moon <mjo970625@gmail.com>
2019-11-06 23:59:19 +0900

    Make a draft of A_Definition

--- A_Definition.cpp
+++ A_Definition.cpp
@@ -1,11 +1,17 @@
-#include <typeinfo>
 #include "A_Definition.h"
+#include <typeinfo>
+#include <iostream>
+
+using std::cout;	using std::endl;
 
 void A_Definition::Print(int d)
 {
 	indent(d);
-	cout<<"definition"<<endl;
-	/*
-	 * 채워넣기
-	 */
+	cout << "definition" << endl;
+
+	indent(d + 2);
+	this->header.Print(d + 2);
+
+	indent(d+2);
+	this->body.Print(d + 2);
 }
