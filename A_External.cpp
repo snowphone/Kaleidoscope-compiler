2621cf9aa56891c5ea9be8203cdfbc41f536d752
Junoh Moon <mjo970625@gmail.com>
2019-11-06 22:45:54 +0900

    Polish A_External

--- A_External.cpp
+++ A_External.cpp
@@ -1,8 +1,11 @@
-#include <typeinfo>
 #include "A_External.h"
+#include <typeinfo>
+#include <iostream>
+
+using std::cout;	using std::endl;
 
 void A_External::Print(int d)
 {
 	indent(d);
-	cout<<"external"<<endl;
+	cout << "external" << endl;
 }
