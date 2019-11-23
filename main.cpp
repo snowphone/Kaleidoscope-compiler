fdbbb3e890b0d14b4255a68f1e34b37f60869aae
Junoh Moon <mjo970625@gmail.com>
2019-11-23 18:36:16 +0900

    Print only IRs, not AST. Succeed to create executable, fail to print IR

--- main.cpp
+++ main.cpp
@@ -21,6 +21,7 @@ using std::vector;	using std::cout;
 
 extern int yyparse(void);
 void StartParse(const char* path);
+void free_tree();
 
 extern FILE* yyin;
 
@@ -33,29 +34,25 @@ int main(int argc, const char* argv[]) {
 
 	TheModule = new llvm::Module(argv[1], getGlobalContext());
 	StartParse(argv[1]);
-	TheModule->print(llvm::outs(), NULL);
+	TheModule->print(llvm::errs(), NULL);
 
+
+	free_tree();
 	return 0;
 } 
 
+void free_tree() {
+	/* Free memory */
+	for(A_TopList::iterator it = aroot->begin(); it != aroot->end(); ++it) {
+		delete *it;
+	}
+	delete aroot;
+}
 
 void StartParse(const char* path) {
 	yyin = fopen(path, "r");
 
-	int result = yyparse();
-	if(result == 0) {
-		cout << "Parsing..." << endl
-			<< 	string(15, '-') << " AST " << string(15, '-') << endl;
-		for(A_TopList::iterator it = aroot->begin(); it != aroot->end(); ++it) {
-			(*it)->Print(0);
-		}
-		cout << string(35, '-') << endl;
-		/* Free memory */
-		for(A_TopList::iterator it = aroot->begin(); it != aroot->end(); ++it) {
-			delete *it;
-		}
-		delete aroot;
-	}
-
+	yyparse();
+	
 	fclose(yyin);
 }
